d432fac0190f8af593044e5d6d06fac07c84ef92
Oye Olalekan Johnson <johnsonoye34@gmail.com>
2024-08-11 22:11:40 +0100

    fix: fixed mailer issue.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def welcome_email(user)
     @full_name = "#{user.first_name} #{user.last_name}"
-    @url  = "#{ENV['BASE_URL']}login"
+    @url  = "#{ENV['BASE_URL']}auth/sign-in"
     mail(to: user.email, subject: 'Welcome to Appstate')
   end
 end
